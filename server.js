800086625d6df9c2474ae6b4ae8ca15d293f35a9
Aukai Tirrell <mtirrell@hawaii.edu>
2017-02-11 18:53:14 -1000

    added req flash capability

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const handlebars = require('express-handlebars');
 const gallery = require('./routes/gallery.js');
 const methodOverride = require('method-override');
+
 
 const app = express();
 
@@ -11,6 +15,9 @@ const hbs = handlebars.create({
   defaultLayout: 'app'
 });
 
+let db = require('./models');
+let Photo = db.Photo;
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
@@ -18,10 +25,15 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-app.use('/gallery', gallery);
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
-let db = require('./models');
-let Photo = db.Photo;
+app.use('/gallery', gallery);
 
 app.get('/', (req, res) => {
     Photo.findAll()
