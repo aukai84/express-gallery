c01ce444de21bcbfda776478736250940d229a66
Aukai Tirrell <mtirrell@hawaii.edu>
2017-02-16 22:07:19 -1000

    authenticate user from User table working kinda

--- server.js
+++ server.js
@@ -58,16 +58,24 @@ passport.use(new LocalStrategy(
   function (username, password, done) {
     console.log('username, password: ', username, password);
     // check if the user is authenticated or not
-    User.findOne({username: username})
-    .then((err, user) => {
-        if(err) {return done(err);}
-        if(!user){
-            return done(null, false, {message: "Incorrect username..."});
-        }
-        if(!user.validPassword(password)){
-            return done(null, false, {message: "Incorrect password..."});
-        }
+    User.findOne({
+        where:
+            {username: username}
+        })
+    .then((user) => {
         return done(null, user);
+        // console.log(user);
+        // if(err) {return done(err);}
+        // if(!user){
+        //     return done(null, false, {message: "Incorrect username..."});
+        // }
+        // if(!user.validPassword(password)){
+        //     return done(null, false, {message: "Incorrect password..."});
+        // }
+        // return done(null, user);
+    })
+    .catch(err => {
+        return done(err);
     });
     // if( authenticate(username, password) ) {
 
