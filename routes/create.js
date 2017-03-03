1a578c34ae1302abf3df7388cdb9b0c1f1593f4c
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-02 19:02:00 -1000

    can't add same username

--- routes/create.js
+++ routes/create.js
@@ -11,19 +11,36 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(req.body.password, salt, function(err, hash) {
-            // Store hash in your password DB.
-            User.create({
-                username: req.body.username,
-                password: hash
-            })
-            .then(user => {
-                req.flash("message", "User created!  Please login!");
-                res.redirect(303, '/login');
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(user => {
+        if(user){
+            req.flash("error", "User already exists!");
+            res.redirect(303, '/create');
+        } else {
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                    // Store hash in your password DB.
+                    User.create({
+                        username: req.body.username,
+                        password: hash
+                    })
+                    .then(user => {
+                        req.flash("message", "User created!  Please login!");
+                        res.redirect(303, '/login');
+                    });
+                 });
             });
-         });
+        }
+    })
+    .catch( error => {
+        req.flash("error", "Error creating user...try again");
+        res.redirect(303, '/create');
     });
+
 });
 
 module.exports = router;
